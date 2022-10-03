1ae8f5c7b33e61f4f52361ad10ca0af4116b1952
Marina <gaspertrishka@mail.ru>
2022-10-03 22:57:19 +0700

    Мак. из 3 чисел

---
+++ Home work02/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+Console.WriteLine("Введите первое число");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третье  число");
+int numberC = int.Parse(Console.ReadLine());
+
+
+if (numberB < numberA && numberA> numberC)
+{
+   Console.WriteLine($"max = {numberA}");
+}
+else
+
+  if (numberA < numberB && numberB> numberC)
+{
+   Console.WriteLine($"max = {numberB}");
+}
+if (numberA < numberC && numberC> numberB)
+{
+   Console.WriteLine($"max = {numberC}");
+}
