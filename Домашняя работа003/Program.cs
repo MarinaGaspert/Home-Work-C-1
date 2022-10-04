9b6fa84dbc201a340a9d868b592249cb6b70ca9d
MarinaGaspert <113772616+MarinaGaspert@users.noreply.github.com>
2022-10-05 00:04:38 +0700

    Update and rename Home work003/Program.cs to Домашняя работа003/Program.cs

---
+++ Домашняя работа003/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+CConsole.WriteLine("Введите  число  ");
+string number = Console.ReadLine();
+int result = int.Parse(number);
+
+if (result  % 2 == 0)
+   Console.WriteLine($"число {result} - четное");
+else
+    Console.WriteLine($"число {result} - нечетное");
